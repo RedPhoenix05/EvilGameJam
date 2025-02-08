73cd9cb95fa96207530fbad7f0f2ab430a929440
Red_Phoenix05 <anson.fry@gmail.com>
2025-02-08 17:03:09 -0600

    Update Interaction Controller

--- Assets/Scripts/InteractionController.cs
+++ Assets/Scripts/InteractionController.cs
@@ -10,6 +10,9 @@ public class InteractionController : MonoBehaviour
     [SerializeField] Transform interactionSource;
     [SerializeField] float maxDistance = 3.0f;
     [SerializeField] Transform attachTransform;
+    [SerializeField] float throwVelocity = 10.0f;
+
+    Interactable selectedItem;
 
 
     void Update()
@@ -19,16 +22,32 @@ public class InteractionController : MonoBehaviour
             bool valid = Physics.Raycast(interactionSource.position, interactionSource.forward, out RaycastHit rayInfo, maxDistance, interactionMask);
             Interactable item = valid ? rayInfo.transform.GetComponentInParent<Interactable>() : null;
             valid &= (bool) item;
-            if (valid)
+            if (valid) // Grabbing ground item
             {
                 GrabItem(item);
+            }
+            else // Throwing hand item
+            {
+                if (selectedItem)
+                {
+                    selectedItem.Throw(transform.forward * throwVelocity);
+                }
             }
         }
     }
 
     public void GrabItem(Interactable item)
     {
+        if (selectedItem)
+        {
+            item.transform.parent = null;
+            item.Drop();
+        }
+
         item.transform.parent = attachTransform;
         item.transform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
+
+        item.Grab();
+        selectedItem = item;
     }
 }
